8d6fef733345591ec3476ceebbaddf8a6ce24047
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-11-04 09:22:20 +0100

    Add benchmarks

---
+++ test/runbenchmarks.jl
@@ -0,0 +1,24 @@
+module BenchmarkingZippedArrays
+
+using ZippedArrays
+using BenchmarkTools
+
+n = 10_000;
+A = rand(Float64, n);
+B = rand(Int64, n);
+C = [(A[i],B[i]) for i in 1:n];
+Z = ZippedArray(A,B);
+C == Z || println("unexpected result!!!")
+
+function sum_first(A::AbstractArray{<:Tuple})
+    s = 0.0
+    @inbounds @simd for i in eachindex(A)
+        s += first(A[i])
+    end
+    return s
+end
+
+@btime sum_first($C) # 1.615 μs (0 allocations: 0 bytes)
+@btime sum_first($Z) # 643.983 ns (0 allocations: 0 bytes)
+
+end # module
